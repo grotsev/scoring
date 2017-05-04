cb3e5b09aafe6109820fe061b45f2cdef67f6884
dgrotsev <dgrotsev@greet-go.com>
2017-05-04 10:30:08 +0300

    Bootstrap client side toolchain

--- bin/postgraphql-scoring.sh
+++ bin/postgraphql-scoring.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-kill -9 $(ps ax | grep postgraph | grep node | cut -d' ' -f1)
-postgraphql --connection postgres://scoring_authenticator:changeme@172.17.0.2:5432/postgres --schema scoring --default-role scoring_superuser
+#postgraphql --connection postgres://scoring_authenticator:changeme@172.17.0.2:5432/postgres --schema scoring --default-role scoring_superuser
+postgraphql --connection postgres://scoring:changeme@172.17.0.2:5432/postgres --schema scoring --default-role scoring
