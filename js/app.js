6ae91bff727a92b6d0de689c06670d09677cfe4f
dgrotsev <dgrotsev@greet-go.com>
2017-05-19 16:21:29 +0300

    Add /dictionary route

--- js/app.js
+++ js/app.js
@@ -15,7 +15,8 @@ import {
 import {
   HashRouter as Router,
   Route,
-  Link
+  Link,
+  NavLink,
 } from 'react-router-dom'
 
 import Authentication from './component/Authentication';
@@ -61,19 +62,44 @@ ReactDOM.render(
   <Router>
     <nav>
       <ul>
-        <li><Link to="/report">Reports</Link>
+        <li><NavLink to="/report">Reports</NavLink>
           <Route path="/report" render={() => (
             <ul>
-              <li><Link to="/report/detail">Detail</Link></li>
-              <li><Link to="/report/rejection">Rejection</Link></li>
-              <li><Link to="/report/service-call">Service calls</Link></li>
-              <li><Link to="/report/app-move">Application movements</Link></li>
-              <li><Link to="/report/app-activity">Application activities</Link></li>
+              <li><NavLink to="/report/detail">Detail</NavLink></li>
+              <li><NavLink to="/report/rejection">Rejection</NavLink></li>
+              <li><NavLink to="/report/service-call">Service calls</NavLink></li>
+              <li><NavLink to="/report/app-move">Application movements</NavLink></li>
+              <li><NavLink to="/report/app-activity">Application activities</NavLink></li>
             </ul>
           )}/>
         </li>
-        <li><Link to="/product-indicator">Product indicator</Link></li>
-        <li><Link to="/dictionary">Dictionary management</Link></li>
+        <li><NavLink to="/product-indicator">Product indicator</NavLink></li>
+        <li><NavLink to="/dictionary">Dictionary management</NavLink>
+          <Route path="/dictionary" render={() => (
+            <ul>
+              <li><NavLink to="/dictionary/country">Страны</NavLink></li>
+              <li><NavLink to="/dictionary/idcard-kind">Документ уд. личн.</NavLink></li>
+              <li><NavLink to="/dictionary/">Должность</NavLink></li>
+              <li><NavLink to="/dictionary/">Ежемесячные расходы, не связанные с получением кредита</NavLink></li>
+              <li><NavLink to="/dictionary/">Иные платежи</NavLink></li>
+              <li><NavLink to="/dictionary/">Кем выдано</NavLink></li>
+              <li><NavLink to="/dictionary/">Курс валюты</NavLink></li>
+              <li><NavLink to="/dictionary/">Отделение</NavLink></li>
+              <li><NavLink to="/dictionary/">Отношение к адресу</NavLink></li>
+              <li><NavLink to="/dictionary/">Отношение к клиенту</NavLink></li>
+              <li><NavLink to="/dictionary/">Отрасль занятости</NavLink></li>
+              <li><NavLink to="/dictionary/">Подписанты</NavLink></li>
+              <li><NavLink to="/dictionary/">Семейное положение</NavLink></li>
+              <li><NavLink to="/dictionary/">Собственный капитал</NavLink></li>
+              <li><NavLink to="/dictionary/">Статус резидентства</NavLink></li>
+              <li><NavLink to="/dictionary/">Тип документа</NavLink></li>
+              <li><NavLink to="/dictionary/">Тип дохода</NavLink></li>
+              <li><NavLink to="/dictionary/">Филиалы</NavLink></li>
+              <li><NavLink to="/dictionary/">Характер занятости</NavLink></li>
+              <li><NavLink to="/dictionary/">Цель кредитования</NavLink></li>
+            </ul>
+          )}/>
+        </li>
       </ul>
     </nav>
   </Router>
