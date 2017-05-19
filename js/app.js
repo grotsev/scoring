ed9e3534359f4b9da254819c9dda6df7b5cbc439
dgrotsev <dgrotsev@greet-go.com>
2017-05-19 16:50:50 +0300

    Extract Navigation component

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import {
   QueryRenderer,
   graphql,
@@ -17,11 +16,12 @@ import {
   Route,
   Link,
   NavLink,
-} from 'react-router-dom'
+} from 'react-router-dom';
 
 import Authentication from './component/Authentication';
 import Authorization from './component/Authorization';
 import Logout from './component/Logout';
+import Navigation from './component/Navigation';
 
 const mountNode = document.getElementById('root');
 
@@ -60,48 +60,14 @@ ReactDOM.render(
     </Authorization>
   </Authentication>*/
   <Router>
-    <nav>
-      <ul>
-        <li><NavLink to="/report">Reports</NavLink>
-          <Route path="/report" render={() => (
-            <ul>
-              <li><NavLink to="/report/detail">Detail</NavLink></li>
-              <li><NavLink to="/report/rejection">Rejection</NavLink></li>
-              <li><NavLink to="/report/service-call">Service calls</NavLink></li>
-              <li><NavLink to="/report/app-move">Application movements</NavLink></li>
-              <li><NavLink to="/report/app-activity">Application activities</NavLink></li>
-            </ul>
-          )}/>
-        </li>
-        <li><NavLink to="/product-indicator">Product indicator</NavLink></li>
-        <li><NavLink to="/dictionary">Dictionary management</NavLink>
-          <Route path="/dictionary" render={() => (
-            <ul>
-              <li><NavLink to="/dictionary/country">Страны</NavLink></li>
-              <li><NavLink to="/dictionary/idcard-kind">Документ уд. личн.</NavLink></li>
-              <li><NavLink to="/dictionary/">Должность</NavLink></li>
-              <li><NavLink to="/dictionary/">Ежемесячные расходы, не связанные с получением кредита</NavLink></li>
-              <li><NavLink to="/dictionary/">Иные платежи</NavLink></li>
-              <li><NavLink to="/dictionary/">Кем выдано</NavLink></li>
-              <li><NavLink to="/dictionary/">Курс валюты</NavLink></li>
-              <li><NavLink to="/dictionary/">Отделение</NavLink></li>
-              <li><NavLink to="/dictionary/">Отношение к адресу</NavLink></li>
-              <li><NavLink to="/dictionary/">Отношение к клиенту</NavLink></li>
-              <li><NavLink to="/dictionary/">Отрасль занятости</NavLink></li>
-              <li><NavLink to="/dictionary/">Подписанты</NavLink></li>
-              <li><NavLink to="/dictionary/">Семейное положение</NavLink></li>
-              <li><NavLink to="/dictionary/">Собственный капитал</NavLink></li>
-              <li><NavLink to="/dictionary/">Статус резидентства</NavLink></li>
-              <li><NavLink to="/dictionary/">Тип документа</NavLink></li>
-              <li><NavLink to="/dictionary/">Тип дохода</NavLink></li>
-              <li><NavLink to="/dictionary/">Филиалы</NavLink></li>
-              <li><NavLink to="/dictionary/">Характер занятости</NavLink></li>
-              <li><NavLink to="/dictionary/">Цель кредитования</NavLink></li>
-            </ul>
-          )}/>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <Navigation/>
+      <main>
+        <Route path="/dictionary/country" render={() =>
+          <div>Ok</div>
+        } />
+      </main>
+    </div>
   </Router>
   ,
   mountNode
---
+++ js/component/Navigation.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  Route,
+  NavLink,
+} from 'react-router-dom';
+
+
+function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/report">Reports</NavLink>
+          <Route path="/report" render={() =>
+            <ul>
+              <li><NavLink to="/report/detail">Detail</NavLink></li>
+              <li><NavLink to="/report/rejection">Rejection</NavLink></li>
+              <li><NavLink to="/report/service-call">Service calls</NavLink></li>
+              <li><NavLink to="/report/app-move">Application movements</NavLink></li>
+              <li><NavLink to="/report/app-activity">Application activities</NavLink></li>
+            </ul>
+          }/>
+        </li>
+        <li><NavLink to="/product-indicator">Product indicator</NavLink></li>
+        <li><NavLink to="/dictionary">Dictionary management</NavLink>
+          <Route path="/dictionary" render={() =>
+            <ul>
+              <li><NavLink to="/dictionary/country">Страны</NavLink></li>
+              <li><NavLink to="/dictionary/idcard-kind">Документ уд. личн.</NavLink></li>
+              <li><NavLink to="/dictionary/">Должность</NavLink></li>
+              <li><NavLink to="/dictionary/">Ежемесячные расходы, не связанные с получением кредита</NavLink></li>
+              <li><NavLink to="/dictionary/">Иные платежи</NavLink></li>
+              <li><NavLink to="/dictionary/">Кем выдано</NavLink></li>
+              <li><NavLink to="/dictionary/">Курс валюты</NavLink></li>
+              <li><NavLink to="/dictionary/">Отделение</NavLink></li>
+              <li><NavLink to="/dictionary/">Отношение к адресу</NavLink></li>
+              <li><NavLink to="/dictionary/">Отношение к клиенту</NavLink></li>
+              <li><NavLink to="/dictionary/">Отрасль занятости</NavLink></li>
+              <li><NavLink to="/dictionary/">Подписанты</NavLink></li>
+              <li><NavLink to="/dictionary/">Семейное положение</NavLink></li>
+              <li><NavLink to="/dictionary/">Собственный капитал</NavLink></li>
+              <li><NavLink to="/dictionary/">Статус резидентства</NavLink></li>
+              <li><NavLink to="/dictionary/">Тип документа</NavLink></li>
+              <li><NavLink to="/dictionary/">Тип дохода</NavLink></li>
+              <li><NavLink to="/dictionary/">Филиалы</NavLink></li>
+              <li><NavLink to="/dictionary/">Характер занятости</NavLink></li>
+              <li><NavLink to="/dictionary/">Цель кредитования</NavLink></li>
+            </ul>
+          }/>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+
+export default Navigation;
