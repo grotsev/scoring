9ec246b3524d1f05fc5775943e1c9e1a8c0d0a85
dgrotsev <dgrotsev@greet-go.com>
2017-05-19 16:03:33 +0300

    Add react-router

--- js/app.js
+++ js/app.js
@@ -12,6 +12,12 @@ import {
   Store,
 } from 'relay-runtime';
 
+import {
+  HashRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
 import Authentication from './component/Authentication';
 import Authorization from './component/Authorization';
 import Logout from './component/Logout';
@@ -47,11 +53,30 @@ const environmentFactory = (token) => {
 };
 
 ReactDOM.render(
-  <Authentication environmentFactory={environmentFactory}>
+  /*<Authentication environmentFactory={environmentFactory}>
     <Authorization environmentFactory={()=>{}} environment={{}} logout={()=>{}}>
       <Logout logout={()=>{}} />
     </Authorization>
-  </Authentication>
+  </Authentication>*/
+  <Router>
+    <nav>
+      <ul>
+        <li><Link to="/report">Reports</Link>
+          <Route path="/report" render={() => (
+            <ul>
+              <li><Link to="/report/detail">Detail</Link></li>
+              <li><Link to="/report/rejection">Rejection</Link></li>
+              <li><Link to="/report/service-call">Service calls</Link></li>
+              <li><Link to="/report/app-move">Application movements</Link></li>
+              <li><Link to="/report/app-activity">Application activities</Link></li>
+            </ul>
+          )}/>
+        </li>
+        <li><Link to="/product-indicator">Product indicator</Link></li>
+        <li><Link to="/dictionary">Dictionary management</Link></li>
+      </ul>
+    </nav>
+  </Router>
   ,
   mountNode
 );
