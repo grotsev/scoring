ab1b2d2759bee4dee3b665eb73e55e99ebcbbef3
dgrotsev <dgrotsev@greet-go.com>
2017-05-26 15:51:00 +0300

    Switch Navigation to bootstrap

--- js/component/Navigation.js
+++ js/component/Navigation.js
@@ -4,51 +4,56 @@ import {
   NavLink,
 } from 'react-router-dom';
 
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
-function Navigation() {
+function Navigation(props) {
   return (
-    <nav>
-      <ul>
-        <li><NavLink to="/report">Reports</NavLink>
-          <Route path="/report" render={() =>
-            <ul>
-              <li><NavLink to="/report/detail">Detail</NavLink></li>
-              <li><NavLink to="/report/rejection">Rejection</NavLink></li>
-              <li><NavLink to="/report/service-call">Service calls</NavLink></li>
-              <li><NavLink to="/report/app-move">Application movements</NavLink></li>
-              <li><NavLink to="/report/app-activity">Application activities</NavLink></li>
-            </ul>
-          }/>
-        </li>
-        <li><NavLink to="/product-indicator">Product indicator</NavLink></li>
-        <li><NavLink to="/dictionary">Dictionary management</NavLink>
-          <Route path="/dictionary" render={() =>
-            <ul>
-              <li><NavLink to="/dictionary/country">Страны</NavLink></li>
-              <li><NavLink to="/dictionary/idcard-kind">Документ уд. личн.</NavLink></li>
-              <li><NavLink to="/dictionary/">Должность</NavLink></li>
-              <li><NavLink to="/dictionary/">Ежемесячные расходы, не связанные с получением кредита</NavLink></li>
-              <li><NavLink to="/dictionary/">Иные платежи</NavLink></li>
-              <li><NavLink to="/dictionary/">Кем выдано</NavLink></li>
-              <li><NavLink to="/dictionary/">Курс валюты</NavLink></li>
-              <li><NavLink to="/dictionary/">Отделение</NavLink></li>
-              <li><NavLink to="/dictionary/">Отношение к адресу</NavLink></li>
-              <li><NavLink to="/dictionary/">Отношение к клиенту</NavLink></li>
-              <li><NavLink to="/dictionary/">Отрасль занятости</NavLink></li>
-              <li><NavLink to="/dictionary/">Подписанты</NavLink></li>
-              <li><NavLink to="/dictionary/">Семейное положение</NavLink></li>
-              <li><NavLink to="/dictionary/">Собственный капитал</NavLink></li>
-              <li><NavLink to="/dictionary/">Статус резидентства</NavLink></li>
-              <li><NavLink to="/dictionary/">Тип документа</NavLink></li>
-              <li><NavLink to="/dictionary/">Тип дохода</NavLink></li>
-              <li><NavLink to="/dictionary/">Филиалы</NavLink></li>
-              <li><NavLink to="/dictionary/">Характер занятости</NavLink></li>
-              <li><NavLink to="/dictionary/">Цель кредитования</NavLink></li>
-            </ul>
-          }/>
-        </li>
-      </ul>
-    </nav>
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href='#'>greetgo! Scoring</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav>
+          <NavDropdown eventKey={1} title='Reports' id='report'>
+            <LinkContainer to="/report/detail"><MenuItem>Detail</MenuItem></LinkContainer>
+            <LinkContainer to="/report/rejection"><MenuItem>Rejection</MenuItem></LinkContainer>
+            <LinkContainer to="/report/service-call"><MenuItem>Service calls</MenuItem></LinkContainer>
+            <LinkContainer to="/report/app-move"><MenuItem>Application movements</MenuItem></LinkContainer>
+            <LinkContainer to="/report/app-activity"><MenuItem>Application activities</MenuItem></LinkContainer>
+          </NavDropdown>
+          <LinkContainer to="/product-indicatory"><NavItem eventKey={2}>Product indicator</NavItem></LinkContainer>
+          <NavDropdown eventKey={3} title='Dictionary management' id='dictionary'>
+            <LinkContainer to="/dictionary/country"><MenuItem>Страны</MenuItem></LinkContainer>
+            <LinkContainer to="/dictionary/idcard-kind"><MenuItem>Документ уд. личн.</MenuItem></LinkContainer>
+            <LinkContainer to="/dictionary/"><MenuItem>Должность</MenuItem></LinkContainer>
+            <LinkContainer to="/dictionary/"><MenuItem>Ежемесячные расходы, не связанные с получением кредита</MenuItem></LinkContainer>
+            <LinkContainer to="/dictionary/"><MenuItem>Иные платежи</MenuItem></LinkContainer>
+            <LinkContainer to="/dictionary/"><MenuItem>Кем выдано</MenuItem></LinkContainer>
+            <LinkContainer to="/dictionary/"><MenuItem>Курс валюты</MenuItem></LinkContainer>
+            <LinkContainer to="/dictionary/"><MenuItem>Отделение</MenuItem></LinkContainer>
+            <LinkContainer to="/dictionary/"><MenuItem>Отношение к адресу</MenuItem></LinkContainer>
+            <LinkContainer to="/dictionary/"><MenuItem>Отношение к клиенту</MenuItem></LinkContainer>
+            <LinkContainer to="/dictionary/"><MenuItem>Отрасль занятости</MenuItem></LinkContainer>
+            <LinkContainer to="/dictionary/"><MenuItem>Подписанты</MenuItem></LinkContainer>
+            <LinkContainer to="/dictionary/"><MenuItem>Семейное положение</MenuItem></LinkContainer>
+            <LinkContainer to="/dictionary/"><MenuItem>Собственный капитал</MenuItem></LinkContainer>
+            <LinkContainer to="/dictionary/"><MenuItem>Статус резидентства</MenuItem></LinkContainer>
+            <LinkContainer to="/dictionary/"><MenuItem>Тип документа</MenuItem></LinkContainer>
+            <LinkContainer to="/dictionary/"><MenuItem>Тип дохода</MenuItem></LinkContainer>
+            <LinkContainer to="/dictionary/"><MenuItem>Филиалы</MenuItem></LinkContainer>
+            <LinkContainer to="/dictionary/"><MenuItem>Характер занятости</MenuItem></LinkContainer>
+            <LinkContainer to="/dictionary/"><MenuItem>Цель кредитования</MenuItem></LinkContainer>
+          </NavDropdown>
+        </Nav>
+        <Nav pullRight>
+          Logout
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
