a264973d00385e1d828082eaa5697a98978aad97
dgrotsev <dgrotsev@greet-go.com>
2017-05-31 15:23:41 +0300

    Add App

--- js/component/App.js
+++ js/component/App.js
@@ -1,4 +1,30 @@
+import React from 'react';
+import * as BS from 'react-bootstrap';
 
 export default function App(props) {
-  return null;
+  return (
+    <BS.Grid fluid>
+      <BS.Row>
+        <BS.Col sm={3}>
+          <BS.PanelGroup>
+
+            <BS.Panel header='Привлечение'>
+              <BS.ListGroup fill>
+                <BS.ListGroupItem>Информация по займу</BS.ListGroupItem>
+                <BS.ListGroupItem>Данные клиента</BS.ListGroupItem>
+                <BS.ListGroupItem>Доходы и расходы</BS.ListGroupItem>
+                <BS.ListGroupItem>Электронное досье</BS.ListGroupItem>
+                <BS.ListGroupItem>Предварительное предложение</BS.ListGroupItem>
+              </BS.ListGroup>
+            </BS.Panel>
+            
+            <BS.Panel header='Заявка'>
+              111
+            </BS.Panel>
+
+          </BS.PanelGroup>
+        </BS.Col>
+      </BS.Row>
+    </BS.Grid>
+  )
 }
