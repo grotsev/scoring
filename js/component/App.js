df118b28f5d3a6212623859974c668fbb4872b5c
dgrotsev <dgrotsev@greet-go.com>
2017-06-07 14:10:27 +0300

    Add reference selection

--- js/component/App.js
+++ js/component/App.js
@@ -42,161 +42,139 @@ export default function App(props) {
             <BS.Panel eventKey='13' header='Регистрация залога' title='2017-02-02 13:42&ndash;14:18 Иванов Иван Иванович'>
             </BS.Panel>
             <BS.Panel eventKey='14' bsStyle='primary' header='Заявка'>
-              <BS.ListGroup>
-                <BS.ListGroupItem header='ИИН'>123456789012</BS.ListGroupItem>
-                <BS.ListGroupItem header='Сервис налогового комитета'>
-                  <BS.ListGroup>
-                    <BS.ListGroupItem header='Фамилия'>Пушкин</BS.ListGroupItem>
-                    <BS.ListGroupItem header='Имя'>Александр</BS.ListGroupItem>
-                    <BS.ListGroupItem header='Отчество'>Сергеевич</BS.ListGroupItem>
-                  </BS.ListGroup>
-                </BS.ListGroupItem>
-                <BS.ListGroupItem header='Фамилия'>Пушкин</BS.ListGroupItem>
-                <BS.ListGroupItem header='Имя'>Александр</BS.ListGroupItem>
-                <BS.ListGroupItem header='Отчество'>Сергеевич</BS.ListGroupItem>
-                <BS.ListGroupItem header='Продукт'>Кредит</BS.ListGroupItem>
-                <BS.ListGroupItem header='Валюта'>KZT</BS.ListGroupItem>
-                <BS.ListGroupItem header='Запрашиваемая сумма'>100&nbsp;000</BS.ListGroupItem>
-                <BS.ListGroupItem>Доходы
-                  <BS.ListGroup>
-                    <BS.ListGroupItem>Оклад за последний месяц: 500</BS.ListGroupItem>
-                    <BS.ListGroupItem>Средняя сумма ежемесячных доходов / по справке о з/п: 500</BS.ListGroupItem>
-                  </BS.ListGroup>
-                </BS.ListGroupItem>
-              </BS.ListGroup>
-              <BS.ButtonToolbar>
-                <BS.Button bsStyle='primary'>Далее</BS.Button>
-                <BS.Button disabled>Завершить работу</BS.Button>
-                <BS.Button>Отменить изменения</BS.Button>
-              </BS.ButtonToolbar>
             </BS.Panel>
-            <BS.Panel eventKey='15' bsStyle='primary' header='Служба безопасности'>
-              <BS.ButtonToolbar>
-                <BS.Button bsStyle='primary'>Взять в работу</BS.Button>
-              </BS.ButtonToolbar>
+            <BS.Panel eventKey='15' bsStyle='primary' header='Служба безопасности' footer={
+                <BS.ButtonToolbar>
+                  <BS.Button bsStyle='primary'>Взять в работу</BS.Button>
+                </BS.ButtonToolbar>
+              }>
             </BS.Panel>
           </BS.PanelGroup>
         </BS.Col>
 
         <BS.Col sm={6}>
-            <TreeView defaultCollapsed={true} nodeLabel={<span className='node'>Информация по займу</span>}>
+            <TreeView defaultCollapsed={true} nodeLabel={<span className='node'>Займ</span>}>
               <div>Продукт</div>
               <div>Запрашиваемая сумма</div>
+              <div><span className='node'><span className='fa fa-fw fa-user' /> Заёмщик</span>
+                <BS.FormControl componentClass='select'>
+                  <option>Нет</option>
+                  <option>Абисов Абис Абисович</option>
+                  <option>Абисова Абисса Абисовна</option>
+                  <option>{'{88141234-1234-5124-9700-000012233445}'}</option>
+                </BS.FormControl>
+                <BS.Button><span className='fa fa-fw fa-plus' /> Создать</BS.Button>
+              </div>
+              <div><span className='node'><span className='fa fa-fw fa-user' /> Созаёмщик</span></div>
+              <div><span className='node'><span className='fa fa-fw fa-user' /> Залогодатель</span></div>
+              <div><span className='node'><span className='fa fa-fw fa-user' /> Залогодатель</span></div>
+              <div><span className='node'><span className='fa fa-fw fa-user' /> Гарант</span></div>
+              <div><span className='node'><span className='fa fa-fw fa-user' /> Страховщик</span></div>
             </TreeView>
 
-            <TreeView defaultCollapsed={false} nodeLabel={<span className='node'><span className='fa fa-fw fa-user' /> Заёмщик</span>}>
-              <TreeView defaultCollapsed={true} nodeLabel={<span className='node'>Лицо</span>}>
-                <span title='Фамилия'>Абисов</span>{' '}
-                <span title='Имя'>Абис</span>{' '}
-                <span title='Отчество'>Абисович</span>{' '}
-                <div title='ИИН'> 123456789012</div>
-                <div title='Дата рождения'>1980.01.02</div>
-                <div title='Семейное положение'>В браке</div>
-                <BS.Image width='96' height='128' title='Фотография' src='images/' />
-              </TreeView>
-              <TreeView defaultCollapsed={true} nodeLabel={<span className='node text-danger' title=' (не подтверждено)'>Адреса</span>}>
-                  <div className='text-success' title='Адрес регистрации (подтверждено)'>
-                    <span className='fa fa-fw fa-id-card' />{' '}
-                    <span>Казахстан</span>,{' '}
-                    <span>Алматинская область</span>,{' '}
-                    <span>Аксуйский район</span>,{' '}
-                    <span>пос. Жансугуров</span><br />
-                    <span>Абая, 25, кв. 2</span>,{' '}
-                    <span>622209</span>
-                  </div>
-                  <div className='text-danger' title='Фактический адрес (не подтверждено)'>
-                    <span className='fa fa-fw fa-home' />{' '}
-                    <span>Казахстан</span>,{' '}
-                    <span>Алматинская область</span>,{' '}
-                    <span>Аксуйский район</span>,{' '}
-                    <span>пос. Жансугуров</span><br />
-                    <span>Абая, 25, кв. 2</span>,{' '}
-                    <span>622209</span>
-                  </div>
-                  <div className='text-warning' title='Рабочий адрес'>
-                    <span className='fa fa-fw fa-industry' />{' '}
-                    <span>Казахстан</span>,{' '}
-                    <span>Алматинская область</span>,{' '}
-                    <span>Аксуйский район</span>,{' '}
-                    <span>пос. Жансугуров</span><br />
-                    <span>Абая, 25, кв. 2</span>,{' '}
-                    <span>622209</span>
-                  </div>
-              </TreeView>
-              <TreeView defaultCollapsed={true} nodeLabel={<span className='node text-danger' title='не подтверждено'>Телефоны</span>}>
-                <div className='text-muted' title='Контактный (не верифицировано)'><span className='fa fa-fw fa-phone' /> +7 701 672 22 22</div>
-                <div className='text-success' title='Домашний (подтверждено)'><span className='fa fa-fw fa-home' /> +7 701 672 22 22</div>
-                <div className='text-danger' title='Мобильный (не подтверждено)'><span className='fa fa-fw fa-mobile' /> +7 777 444 90 24</div>
-                <div className='text-warning' title='Рабочий (сомнительно)'><span className='fa fa-fw fa-industry' /> +7 777 444 90 24</div>
-              </TreeView>
-              <TreeView defaultCollapsed={true} nodeLabel={<span className='node'>Доходы</span>}>
-                <div>Оклад за последний месяц: 500</div>
-                <div>Средняя сумма ежемесячных доходов / Средняя сумма по справке о з/п: 500</div>
-                <div>Ежемесячный доход супруга/ги: 500</div>
-                <div>Дополнительный подтверждённый доход: 500</div>
-              </TreeView>
-              <TreeView defaultCollapsed={true} nodeLabel={<span className='node'>Расходы</span>}>
-                <div>По действующим кредитам</div>
-                <div>Сумма КЛ на кредитных картах</div>
-                <div>Расходы на образование (детский сад, школа, институт)</div>
-                <div>Аренда жилья</div>
-                <div>Расходы на коммунальные услуги</div>
-                <div>Расходы на услуги мобильной связи, Интернет и т.п.</div>
-                <div>Личные платежи</div>
-                <div>Алименты</div>
-                <div>Прочие расходы (лечение, помошь родственникам)</div>
-                <div>Страхование</div>
-                <div>Комиссии</div>
-                <div>Годовой налог на транспорт</div>
-                <div>Итого расходов в месяц</div>
-              </TreeView>
-              <TreeView defaultCollapsed={false} nodeLabel={<span className='node text-muted' title='не верифицировано'>Данные о занятости</span>}>
-              </TreeView>
-              <TreeView defaultCollapsed={false} nodeLabel={<span className='node'>Контактные лица</span>}>
-                <TreeView defaultCollapsed={true} nodeLabel={<span className='node'>Супруг</span>}>
-                  <span title='Фамилия'>Абисова</span>{' '}
-                  <span title='Имя'>Абисса</span>{' '}
-                  <span title='Отчество'>Абисовна</span>{' '}
-                  <div title='Контактный'><span className='fa fa-fw fa-phone' /> +7 701 672 22 22</div>
-                  <div title='Домашний'><span className='fa fa-fw fa-home' /> +7 701 672 22 22</div>
-                  <div title='Мобильный'><span className='fa fa-fw fa-mobile' /> +7 777 444 90 24</div>
-                  <div title='Рабочий'><span className='fa fa-fw fa-industry' /> +7 777 444 90 24</div>
+            <TreeView defaultCollapsed={false} nodeLabel={<span className='node'><span className='fa fa-fw fa-user' /> Лица</span>}>
+              <TreeView defaultCollapsed={false} nodeLabel={<span className='node'><span className='fa fa-fw fa-user' /> Абисов Абис Абисович</span>}>
+                <TreeView defaultCollapsed={true} nodeLabel={<span className='node'>Лицо</span>}>
+                  <span title='Фамилия'>Абисов</span>{' '}
+                  <span title='Имя'>Абис</span>{' '}
+                  <span title='Отчество'>Абисович</span>{' '}
+                  <div title='ИИН'> 123456789012</div>
+                  <div title='Дата рождения'>1980.01.02</div>
+                  <div title='Семейное положение'>В браке</div>
+                  <BS.Image width='96' height='128' title='Фотография' src='images/' />
                 </TreeView>
-                <TreeView defaultCollapsed={true} nodeLabel={<span className='node'>Родитель</span>}>
+                <TreeView defaultCollapsed={false} nodeLabel={<span className='node'><span className='fa fa-fw fa-home' /> Адреса</span>}>
+                    <div>
+                      <span className='fa fa-fw fa-home' /> Адрес регистрации
+                      <BS.FormControl componentClass='select'>
+                        <option>Нет</option>
+                        <option>пос. Жансугуров, ул. Абая, 25, кв.2</option>
+                        <option>пос. Жансугуров, ул. Абая, 12, кв.13</option>
+                        <option>{'{88141234-1234-5124-9700-000012233445}'}</option>
+                      </BS.FormControl>
+                      <BS.Button><span className='fa fa-fw fa-plus' /> Создать</BS.Button>
+                    </div>
+                    <div><span className='fa fa-fw fa-home' /> Фактический адрес <a href='#'>пос. Жансугуров, ул. Абая, 25, кв.2</a></div>
+                    <div><span className='fa fa-fw fa-home' /> Рабочий адрес <a href='#'>пос. Жансугуров, ул. Абая, 12, кв.13</a></div>
                 </TreeView>
-                <TreeView defaultCollapsed={true} nodeLabel={<span className='node'>Ребёнок</span>}>
+                <TreeView defaultCollapsed={true} nodeLabel={<span className='node text-danger' title='не подтверждено'>Телефоны</span>}>
+                  <div className='text-muted' title='Контактный (не верифицировано)'><span className='fa fa-fw fa-phone' /> +7 701 672 22 22</div>
+                  <div className='text-success' title='Домашний (подтверждено)'><span className='fa fa-fw fa-home' /> +7 701 672 22 22</div>
+                  <div className='text-danger' title='Мобильный (не подтверждено)'><span className='fa fa-fw fa-mobile' /> +7 777 444 90 24</div>
+                  <div className='text-warning' title='Рабочий (сомнительно)'><span className='fa fa-fw fa-industry' /> +7 777 444 90 24</div>
                 </TreeView>
-                <TreeView defaultCollapsed={true} nodeLabel={<span className='node'>Ребёнок</span>}>
+                <TreeView defaultCollapsed={true} nodeLabel={<span className='node'>Доходы</span>}>
+                  <div>Оклад за последний месяц: 500</div>
+                  <div>Средняя сумма ежемесячных доходов / Средняя сумма по справке о з/п: 500</div>
+                  <div>Ежемесячный доход супруга/ги: 500</div>
+                  <div>Дополнительный подтверждённый доход: 500</div>
                 </TreeView>
-                <TreeView defaultCollapsed={true} nodeLabel={<span className='node'>Коллега</span>}>
+                <TreeView defaultCollapsed={true} nodeLabel={<span className='node'>Расходы</span>}>
+                  <div>По действующим кредитам</div>
+                  <div>Сумма КЛ на кредитных картах</div>
+                  <div>Расходы на образование (детский сад, школа, институт)</div>
+                  <div>Аренда жилья</div>
+                  <div>Расходы на коммунальные услуги</div>
+                  <div>Расходы на услуги мобильной связи, Интернет и т.п.</div>
+                  <div>Личные платежи</div>
+                  <div>Алименты</div>
+                  <div>Прочие расходы (лечение, помошь родственникам)</div>
+                  <div>Страхование</div>
+                  <div>Комиссии</div>
+                  <div>Годовой налог на транспорт</div>
+                  <div>Итого расходов в месяц</div>
                 </TreeView>
-                <TreeView defaultCollapsed={true} nodeLabel={<span className='node'>Друг</span>}>
+                <TreeView defaultCollapsed={true} nodeLabel={<span className='node text-muted' title='не верифицировано'>Данные о занятости</span>}>
                 </TreeView>
+                <TreeView defaultCollapsed={true} nodeLabel={<span className='node'>Контактные лица</span>}>
+                  <TreeView defaultCollapsed={true} nodeLabel={<span className='node'>Супруг</span>}>
+                    <span title='Фамилия'>Абисова</span>{' '}
+                    <span title='Имя'>Абисса</span>{' '}
+                    <span title='Отчество'>Абисовна</span>{' '}
+                    <div title='Контактный'><span className='fa fa-fw fa-phone' /> +7 701 672 22 22</div>
+                    <div title='Домашний'><span className='fa fa-fw fa-home' /> +7 701 672 22 22</div>
+                    <div title='Мобильный'><span className='fa fa-fw fa-mobile' /> +7 777 444 90 24</div>
+                    <div title='Рабочий'><span className='fa fa-fw fa-industry' /> +7 777 444 90 24</div>
+                  </TreeView>
+                  <TreeView defaultCollapsed={true} nodeLabel={<span className='node'>Родитель</span>}>
+                  </TreeView>
+                  <TreeView defaultCollapsed={true} nodeLabel={<span className='node'>Ребёнок</span>}>
+                  </TreeView>
+                  <TreeView defaultCollapsed={true} nodeLabel={<span className='node'>Ребёнок</span>}>
+                  </TreeView>
+                  <TreeView defaultCollapsed={true} nodeLabel={<span className='node'>Коллега</span>}>
+                  </TreeView>
+                  <TreeView defaultCollapsed={true} nodeLabel={<span className='node'>Друг</span>}>
+                  </TreeView>
+                </TreeView>
+              </TreeView>
+
+            </TreeView>
+
+            <TreeView defaultCollapsed={true} nodeLabel={<span className='node'><span className='fa fa-fw fa-home' />Адреса</span>}>
+              <TreeView defaultCollapsed={true} nodeLabel={<span className='node'><span className='fa fa-fw fa-home' /> пос. Жансугуров, ул. Абая, 25, кв.2</span>}>
+                <div>Казахстан</div>
+                <div>Алматинская область</div>
+                <div>Аксуйский район</div>
+                <div>пос. Жансугуров</div>
+                <div>Абая</div>
+                <div>25</div>
+                <div>кв. 2</div>
+                <div>622209</div>
+              </TreeView>
+              <TreeView defaultCollapsed={true} nodeLabel={<span className='node'><span className='fa fa-fw fa-home' /> пос. Жансугуров, ул. Абая, 12, кв.13</span>}>
+                <div>Казахстан</div>
+                <div>Алматинская область</div>
+                <div>Аксуйский район</div>
+                <div>пос. Жансугуров</div>
+                <div>Абая</div>
+                <div>12</div>
+                <div>кв. 13</div>
+                <div>622209</div>
               </TreeView>
             </TreeView>
 
-            <TreeView defaultCollapsed={true} nodeLabel={<span className='node'><span className='fa fa-fw fa-user' /> Созаёмщик</span>}>
-            </TreeView>
-
-            <TreeView defaultCollapsed={true} nodeLabel={<span className='node'><span className='fa fa-fw fa-user' /> Залогодатель</span>}>
-            </TreeView>
-
-            <TreeView defaultCollapsed={true} nodeLabel={<span className='node'><span className='fa fa-fw fa-user' /> Залогодатель</span>}>
-            </TreeView>
-
-            <TreeView defaultCollapsed={true} nodeLabel={<span className='node'><span className='fa fa-fw fa-user' /> Гарант</span>}>
-            </TreeView>
-
-            <TreeView defaultCollapsed={true} nodeLabel={<span className='node'><span className='fa fa-fw fa-user' /> Страховщик</span>}>
-            </TreeView>
-
-            <TreeView defaultCollapsed={true} nodeLabel={<span className='node'><span className='fa fa-fw fa-users' /> Лица</span>}>
-            </TreeView>
-
-            <TreeView defaultCollapsed={true} nodeLabel={<span className='node'>Адреса</span>}>
-            </TreeView>
-
-            <TreeView defaultCollapsed={true} nodeLabel={<span className='node'>Электронное досье</span>}>
+            <TreeView defaultCollapsed={true} nodeLabel={<span className='node'><span className='fa fa-fw fa-file' />Электронное досье</span>}>
               <div>4</div>
               <div>5</div>
             </TreeView>
