989e9ce16d8d912d1ad1d7d839623a0543655c8e
dgrotsev <dgrotsev@greet-go.com>
2017-06-08 13:48:09 +0300

    Switch App to routing

--- js/component/App.js
+++ js/component/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as BS from 'react-bootstrap';
 import TreeView from 'react-treeview';
+import { LinkContainer } from 'react-router-bootstrap';
 
 
 export default function App(props) {
@@ -11,25 +12,25 @@ export default function App(props) {
         <BS.Col sm={3}>
           <BS.Panel>
             <BS.ListGroup fill>
-              <BS.ListGroupItem>Привлечение</BS.ListGroupItem>
-              <BS.ListGroupItem>Заявка</BS.ListGroupItem>
-              <BS.ListGroupItem>Верификация</BS.ListGroupItem>
-              <BS.ListGroupItem>Заявка</BS.ListGroupItem>
-              <BS.ListGroupItem>Оценка залогового имущества</BS.ListGroupItem>
-              <BS.ListGroupItem>Юридическая проверка</BS.ListGroupItem>
-              <BS.ListGroupItem>Служба безопасности</BS.ListGroupItem>
-              <BS.ListGroupItem>Управление кредитными рисками</BS.ListGroupItem>
-              <BS.ListGroupItem>КК по розничному бизнесу</BS.ListGroupItem>
-              <BS.ListGroupItem>Кредитный комитет филиала</BS.ListGroupItem>
-              <BS.ListGroupItem>Дополнительная юридическая экспертиза</BS.ListGroupItem>
-              <BS.ListGroupItem>Подписание договора</BS.ListGroupItem>
-              <BS.ListGroupItem>Регистрация залога</BS.ListGroupItem>
-              <BS.ListGroupItem active>Заявка</BS.ListGroupItem>
-              <BS.ListGroupItem>Служба безопасности
+              <LinkContainer to="/app/1/stage/20170506102030"><BS.ListGroupItem>Привлечение</BS.ListGroupItem></LinkContainer>
+              <LinkContainer to="/app/1/stage/20170506102130"><BS.ListGroupItem>Заявка</BS.ListGroupItem></LinkContainer>
+              <LinkContainer to="/app/1/stage/20170506102230"><BS.ListGroupItem>Верификация</BS.ListGroupItem></LinkContainer>
+              <LinkContainer to="/app/1/stage/20170506102330"><BS.ListGroupItem>Заявка</BS.ListGroupItem></LinkContainer>
+              <LinkContainer to="/app/1/stage/20170506102430"><BS.ListGroupItem>Оценка залогового имущества</BS.ListGroupItem></LinkContainer>
+              <LinkContainer to="/app/1/stage/20170506102530"><BS.ListGroupItem>Юридическая проверка</BS.ListGroupItem></LinkContainer>
+              <LinkContainer to="/app/1/stage/20170506102630"><BS.ListGroupItem>Служба безопасности</BS.ListGroupItem></LinkContainer>
+              <LinkContainer to="/app/1/stage/20170506102730"><BS.ListGroupItem>Управление кредитными рисками</BS.ListGroupItem></LinkContainer>
+              <LinkContainer to="/app/1/stage/20170506102830"><BS.ListGroupItem>КК по розничному бизнесу</BS.ListGroupItem></LinkContainer>
+              <LinkContainer to="/app/1/stage/20170506102930"><BS.ListGroupItem>Кредитный комитет филиала</BS.ListGroupItem></LinkContainer>
+              <LinkContainer to="/app/1/stage/20170506103030"><BS.ListGroupItem>Дополнительная юридическая экспертиза</BS.ListGroupItem></LinkContainer>
+              <LinkContainer to="/app/1/stage/20170506103130"><BS.ListGroupItem>Подписание договора</BS.ListGroupItem></LinkContainer>
+              <LinkContainer to="/app/1/stage/20170506103230"><BS.ListGroupItem>Регистрация залога</BS.ListGroupItem></LinkContainer>
+              <LinkContainer to="/app/1/stage/20170506103330"><BS.ListGroupItem>Заявка</BS.ListGroupItem></LinkContainer>
+              <LinkContainer to="/app/1/stage/20170506103430"><BS.ListGroupItem>Служба безопасности
                 <BS.ButtonToolbar>
-                  <BS.Button bsStyle='primary'>Взять в работу</BS.Button>
+                  <BS.Button>Взять в работу</BS.Button>
                 </BS.ButtonToolbar>
-              </BS.ListGroupItem>
+              </BS.ListGroupItem></LinkContainer>
             </BS.ListGroup>
           </BS.Panel>
         </BS.Col>
