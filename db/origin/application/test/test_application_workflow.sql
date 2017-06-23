2579256eb226ba72a61fbf5a4c8031fc48da8eda
dgrotsev <dgrotsev@greet-go.com>
2017-06-23 18:10:12 +0300

    Add test_application_workflow

---
+++ db/origin/application/test/test_application_workflow.sql
@@ -0,0 +1,30 @@
+create or replace function test_application_workflow() returns setof text as $function$
+declare
+  the_application uuid;
+  the_stage code;
+  cursor refcursor;
+begin
+  set local jwt.claims.staff = '11110000-0000-0000-0000-000011110000';
+  
+  set local role to scoring_attraction;
+  select application_create() into the_application;
+  return next isnt(the_application , null, 'application_create should create new application');
+
+  reset role;
+  open cursor for
+    select stage
+    from application_stage
+    where application = the_application
+  ;
+  return next results_eq
+    ( cursor
+    , array['attraction']
+    , 'New application should be in [attraction] stage'
+    );
+  close cursor;
+
+end;
+$function$ language plpgsql
+  set role from current
+;
+
