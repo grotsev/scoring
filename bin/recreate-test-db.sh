ac7bbf372127e38b300afcc470616eae1be2674b
dgrotsev <dgrotsev@greet-go.com>
2017-07-10 13:51:08 +0300

    Combine recreate-test-db.sh into test.sh

--- bin/recreate-test-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-bin/build-db.sh
-psql postgres://postgres:111@172.17.0.2:5432/postgres -q1 --file db/init/init-clean.sql && \
-psql postgres://postgres:111@172.17.0.2:5432/postgres -q1 --file db/init/init-model.sql && \
-psql postgres://scoring:changeme@172.17.0.2:5432/postgres -q1 --file build/db.sql && \
-psql postgres://postgres:111@172.17.0.2:5432/postgres -q1 --file db/init/init-test.sql && \
-psql postgres://scoring:changeme@172.17.0.2:5432/postgres -q1 --file build/db-test.sql && \
-psql postgres://postgres:111@172.17.0.2:5432/postgres -q1 --file db/init/init-test-grant.sql && \
-psql postgres://authenticator:changeme@172.17.0.2:5432/postgres --tuples-only -c "select runtests()"
