7f1e819429089c3386411906fb2368be420e97ff
dgrotsev <dgrotsev@greet-go.com>
2017-07-12 11:07:55 +0300

    Add test-one.sh

---
+++ bin/test-one.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+bin/recreate-db.sh \
+echo "TAP version 13" && \
+psql postgres://authenticator:changeme@172.17.0.2:5432/postgres --tuples-only -c "select runtests('^test_application_workflow$')"
