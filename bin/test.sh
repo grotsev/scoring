5ae37f69cd2535807a0838e71ba85838614d8632
dgrotsev <dgrotsev@greet-go.com>
2017-07-24 13:55:25 +0300

    Unify test.sh and test-one.sh

--- bin/test.sh
+++ bin/test.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 bin/recreate-db.sh \
 echo "TAP version 13" && \
-psql postgres://authenticator:changeme@172.17.0.2:5432/postgres --tuples-only -c "select runtests()"
+if [[ $# -eq 0 ]]; then
+  psql postgres://authenticator:changeme@172.17.0.2:5432/postgres --tuples-only -c "select runtests()"
+else
+  for test in $*; do
+    psql postgres://authenticator:changeme@172.17.0.2:5432/postgres --tuples-only -c "select runtests('^$test$')"
+  done
+fi
