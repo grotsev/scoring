5e3c7d5537682f6d509aac3af9f022922049d383
dgrotsev <dgrotsev@greet-go.com>
2017-07-31 15:26:35 +0300

    Add rework table

--- db/model/model-application.sql
+++ db/model/model-application.sql
@@ -62,6 +62,19 @@ create trigger pin_versioning
   for each row execute procedure versioning('sys_period', 'pin_history', true)
 ;
 
+-- TODO reset
+create table rework
+( application uuid not null
+, back        code not null
+, forth       code not null
+
+, primary key (application)
+, foreign key (back)  references stage
+, foreign key (forth) references stage
+);
+
+comment on table rework is
+  'Fix mistakes in back stage and return forth. Other stages are untouched';
 
 
 create function assign_outlet(
