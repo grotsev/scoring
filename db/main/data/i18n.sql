d75d3425d57cb9f9fa3a0ab814a6c0e5c5e17856
dgrotsev <dgrotsev@greet-go.com>
2017-08-11 18:43:19 +0300

    Add i18n.i18n_rus

--- db/main/data/i18n.sql
+++ db/main/data/i18n.sql
@@ -1,3 +1,10 @@
-insert into i18n values
-  ('RUS')
+-- add manually because macro_i18n can not be recursive
+alter table i18n
+  add column i18n_rus text not null
+;
+
+insert into i18n
+  (i18n , i18n_rus )
+values
+  ('RUS', 'русский')
 ;
