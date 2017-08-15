2b8917d71785cf8a535ecb84ef3504b0280c22a2
dgrotsev <dgrotsev@greet-go.com>
2017-08-15 16:07:45 +0300

    Restore become

--- bin/db-become.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-psql postgres://authenticator:changeme@172.17.0.2:5432/postgres --tuples-only -c "select become(auth('all', '$1'), true);"
--- db/test/function/become.sql
+++ db/test/function/become.sql
@@ -1,22 +1,19 @@
 create function become(
-  token jwt_token,
-  global boolean = false
+  token jwt_token
 ) returns uuid
   language plpgsql
 as $function$
 declare
   cmd text;
 begin
-  cmd = case when global then 'set' else 'set local' end;
-
-  execute cmd||$$ jwt.claims.login = '$$ || token.login || $$'$$;
-  execute cmd||$$ jwt.claims.role  = '$$ || token.role  || $$'$$;
-  execute cmd||$$ jwt.claims.actor = '$$ || token.actor || $$'$$;
-  execute cmd||$$ role $$ || token.role;
+  execute $$set local jwt.claims.login = '$$ || token.login || $$'$$;
+  execute $$set local jwt.claims.role  = '$$ || token.role  || $$'$$;
+  execute $$set local jwt.claims.actor = '$$ || token.actor || $$'$$;
+  execute $$set local role $$ || token.role;
 
   return token.actor;
 end;
 $function$;
 
-comment on function become(jwt_token,boolean) is
+comment on function become(jwt_token) is
   'Become a actor and role';
