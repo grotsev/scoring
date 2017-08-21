f58290abff0667dff71c92736f19c29a865adcf0
dgrotsev <dgrotsev@greet-go.com>
2017-08-21 13:32:32 +0300

    Add elm routing

---
+++ elm/src/Main.elm
@@ -0,0 +1,201 @@
+module Main exposing (..)
+
+import Bootstrap.Button as Button
+import Bootstrap.Card as Card
+import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
+import Bootstrap.ListGroup as Listgroup
+import Bootstrap.Modal as Modal
+import Bootstrap.Navbar as Navbar
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+import Navigation exposing (Location)
+import Routing exposing (..)
+
+
+main : Program Never Model Msg
+main =
+    Navigation.program UrlChange
+        { view = view
+        , update = update
+        , subscriptions = subscriptions
+        , init = init
+        }
+
+
+type alias Model =
+    { route : Route
+    , navState : Navbar.State
+    , modalState : Modal.State
+    }
+
+
+init : Location -> ( Model, Cmd Msg )
+init location =
+    let
+        ( navState, navCmd ) =
+            Navbar.initialState NavMsg
+
+        ( model, urlCmd ) =
+            urlUpdate location { navState = navState, route = Home, modalState = Modal.hiddenState }
+    in
+    ( model, Cmd.batch [ urlCmd, navCmd ] )
+
+
+type Msg
+    = UrlChange Location
+    | NavMsg Navbar.State
+    | ModalMsg Modal.State
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Navbar.subscriptions model.navState NavMsg
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        UrlChange location ->
+            urlUpdate location model
+
+        NavMsg state ->
+            ( { model | navState = state }
+            , Cmd.none
+            )
+
+        ModalMsg state ->
+            ( { model | modalState = state }
+            , Cmd.none
+            )
+
+
+urlUpdate : Navigation.Location -> Model -> ( Model, Cmd Msg )
+urlUpdate location model =
+    ( { model | route = parseLocation location }, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ menu model
+        , mainContent model
+        , modal model
+        ]
+
+
+menu : Model -> Html Msg
+menu model =
+    Navbar.config NavMsg
+        |> Navbar.withAnimation
+        |> Navbar.container
+        |> Navbar.brand [ href "#" ] [ text "Elm Bootstrap" ]
+        |> Navbar.items
+            [ Navbar.itemLink [ href "#getting-started" ] [ text "Getting started" ]
+            , Navbar.itemLink [ href "#modules" ] [ text "Modules" ]
+            ]
+        |> Navbar.view model.navState
+
+
+mainContent : Model -> Html Msg
+mainContent model =
+    Grid.container [] <|
+        case model.route of
+            Home ->
+                routeHome model
+
+            ApplicationList ->
+                routeGettingStarted model
+
+            Dictionary ->
+                routeModules model
+
+            NotFound ->
+                routeNotFound
+
+            _ ->
+                Debug.crash "TODO"
+
+
+routeHome : Model -> List (Html Msg)
+routeHome model =
+    [ h1 [] [ text "Home" ]
+    , Grid.row []
+        [ Grid.col []
+            [ Card.config [ Card.outlinePrimary ]
+                |> Card.headerH4 [] [ text "Getting started" ]
+                |> Card.block []
+                    [ Card.text [] [ text "Getting started is real easy. Just click the start button." ]
+                    , Card.custom <|
+                        Button.linkButton
+                            [ Button.primary, Button.attrs [ href "#getting-started" ] ]
+                            [ text "Start" ]
+                    ]
+                |> Card.view
+            ]
+        , Grid.col []
+            [ Card.config [ Card.outlineDanger ]
+                |> Card.headerH4 [] [ text "Modules" ]
+                |> Card.block []
+                    [ Card.text [] [ text "Check out the modules overview" ]
+                    , Card.custom <|
+                        Button.linkButton
+                            [ Button.primary, Button.attrs [ href "#modules" ] ]
+                            [ text "Module" ]
+                    ]
+                |> Card.view
+            ]
+        ]
+    ]
+
+
+routeGettingStarted : Model -> List (Html Msg)
+routeGettingStarted model =
+    [ h2 [] [ text "Getting started" ]
+    , Button.button
+        [ Button.success
+        , Button.large
+        , Button.block
+        , Button.attrs [ onClick <| ModalMsg Modal.visibleState ]
+        ]
+        [ text "Click me" ]
+    ]
+
+
+routeModules : Model -> List (Html Msg)
+routeModules model =
+    [ h1 [] [ text "Modules" ]
+    , Listgroup.ul
+        [ Listgroup.li [] [ text "Alert" ]
+        , Listgroup.li [] [ text "Badge" ]
+        , Listgroup.li [] [ text "Card" ]
+        ]
+    ]
+
+
+routeNotFound : List (Html Msg)
+routeNotFound =
+    [ h1 [] [ text "Not found" ]
+    , text "SOrry couldn't find that route"
+    ]
+
+
+modal : Model -> Html Msg
+modal model =
+    Modal.config ModalMsg
+        |> Modal.small
+        |> Modal.h4 [] [ text "Getting started ?" ]
+        |> Modal.body []
+            [ Grid.containerFluid []
+                [ Grid.row []
+                    [ Grid.col
+                        [ Col.xs6 ]
+                        [ text "Col 1" ]
+                    , Grid.col
+                        [ Col.xs6 ]
+                        [ text "Col 2" ]
+                    ]
+                ]
+            ]
+        |> Modal.view model.modalState
